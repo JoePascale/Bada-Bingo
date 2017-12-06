0f23f16cc671e6870c71819a903ce16ef003e823
JoePascale <joe.m.pascale@gmail.com>
2017-12-06 10:34:59 -0800

    generated event model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204203526) do
+ActiveRecord::Schema.define(version: 20171206182414) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "registered_applications_id"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "events", ["registered_applications_id"], name: "index_events_on_registered_applications_id"
 
   create_table "registered_applications", force: :cascade do |t|
     t.string   "name"
