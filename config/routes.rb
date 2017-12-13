c10ca98a8171faa458a4db1e4ccc74f6d681ce05
JoePascale <joe.m.pascale@gmail.com>
2017-12-13 09:26:38 -0800

    added API controller associated files

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
 
+  namespace :api do
+  get 'events/create'
+  end
+
   devise_for :users
 
   resources :registered_applications
 
   root 'welcome#index'
 
+  namespace :api, defaults: { format: :json } do
+    resources :events, only: [:create]
+  end
 
-  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
